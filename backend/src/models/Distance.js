8c1c39ce8c463c025e75608d4e3870f680e962b0
bradh43 <brad.hodkinson2@gmail.com>
2020-10-27 19:05:00 -0500

    changes to repo structure

--- backend/src/models/Distance.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from 'mongoose'
-
-export const DistanceSchema = Schema({
-  value: Number,
-  unit: String
-})
-
-export default mongoose.model('Distance', DistanceSchema)
--- backend/src/typeDefs/SuccessMessage.js
+++
@@ -1,9 +0,0 @@
-import { gql } from 'apollo-server-express'
-
-export default gql`
-
-  type SuccessMessage {
-    message: String!
-    success: Boolean!
-  }
-`
