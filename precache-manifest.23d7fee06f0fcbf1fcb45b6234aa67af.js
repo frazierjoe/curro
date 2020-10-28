9871fdadf64df6e37cb2cda9a46efd69bdea6df2
evanhan6561 <54378519+evanhan6561@users.noreply.github.com>
2020-10-27 23:04:25 -0500

    Updates

---
+++ precache-manifest.23d7fee06f0fcbf1fcb45b6234aa67af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35e01ba170ca1f69ab4f411c7372ad0",
+    "url": "/curro/index.html"
+  },
+  {
+    "revision": "7dfe6d8815055b35df9e",
+    "url": "/curro/static/css/main.458a1203.chunk.css"
+  },
+  {
+    "revision": "8ec75da32e9dae838059",
+    "url": "/curro/static/js/2.9aae11b6.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/curro/static/js/2.9aae11b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfe6d8815055b35df9e",
+    "url": "/curro/static/js/main.cf9258ce.chunk.js"
+  },
+  {
+    "revision": "ce53dc4fddfff56d06df",
+    "url": "/curro/static/js/runtime-main.3b77693f.js"
+  }
+]);
