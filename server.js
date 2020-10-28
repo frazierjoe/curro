4bf8395c3fee3cfff09d99c11e712577a72ba31d
evanhan6561 <54378519+evanhan6561@users.noreply.github.com>
2020-10-27 23:24:53 -0500

    Back to trying Heroku v2

---
+++ server.js
@@ -0,0 +1,12 @@
+// https://stackoverflow.com/questions/60909911/how-do-i-deploy-a-static-react-app-to-heroku?fbclid=IwAR3qVxxn3qBbKs6TuY-hu5jvd-5CVonENxKOQZd7VYQDT5914YgAY8BqzcE
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT);
